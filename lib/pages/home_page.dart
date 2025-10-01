0466ff2e17842fe003a8ea173c8f011f6cff1ffb
jiabinxu <90496448+JiaBinxu33@users.noreply.github.com>
2025-10-01 16:14:42 +0000

    首页练习，路由跳转

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codespace_practice/pages/web_view_page.dart';
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+              height: 150.h,
+              width: double.infinity,
+              child: Swiper(
+                itemCount: 3,
+                autoplay: true,
+                pagination: const SwiperPagination(),
+                control: const SwiperControl(),
+                indicatorLayout: PageIndicatorLayout.NONE,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 150,
+                    color: Colors.blue,
+                  );
+                },
+              )),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (context, index) {
+              return _listItemView();
+            },
+            itemCount: 10,
+          ))
+        ],
+      )),
+    );
+  }
+
+  Widget _listItemView() {
+    return GestureDetector(onTap: () => {
+      Navigator.push(context, MaterialPageRoute(builder: (context){
+        return WebViewPage(title: "webView",);
+      }))
+    },child: Container(
+        margin: EdgeInsets.only(left: 10.w, right: 10.w, top: 5.h, bottom: 5.h),
+        padding:
+            EdgeInsets.only(left: 10.w, right: 10.w, top: 5.h, bottom: 5.h),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.black12, width: 0.5),
+            borderRadius: BorderRadius.all(Radius.circular(6.r))),
+        child: Column(children: [
+          Row(
+            // padding:16.r,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15.r),
+                child: Image.asset(
+                  "assets/images/luoxiaohei.png",
+                  width: 30.r,
+                  height: 30.r,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
+              Text(
+                "作者",
+                style: TextStyle(color: Colors.black),
+              ),
+              Expanded(child: SizedBox()),
+              Text("2025年10月1日20:46:41", style: TextStyle(color: Colors.black)),
+              SizedBox(
+                width: 5.w,
+              ),
+              Text("置顶",
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Text(
+              "Recompile complete. Page requires refresh.Performing hot restart...34msRestarted application in 35ms.",
+              style: TextStyle(color: Colors.black, fontSize: 14.sp)),
+          Row(
+            children: [
+              Text("干货资源",
+                  style: TextStyle(color: Colors.green, fontSize: 12.sp)),
+              Expanded(child: SizedBox()),
+              Image.asset(
+                "assets/images/img_collect_grey.png",
+                width: 30.r,
+                height: 30.r,
+              )
+            ],
+          )
+        ])),);
+  }
+}
---
+++ lib/pages/web_view_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_codespace_practice/main.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class WebViewPage extends StatefulWidget {
+  final String title;
+  const WebViewPage({super.key, required this.title});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _WebViewPageState();
+  }
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(child: Container(child: 
+        InkWell(onTap: (){
+          Navigator.pop(context);
+        },child: Container(width: 200.w,height: 50.h,child: Text("返回"),),)
+      ,)),
+    );
+  }
+}
